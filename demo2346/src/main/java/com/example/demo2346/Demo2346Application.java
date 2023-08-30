17ce1c36d58f7a5017b9176e145858e4528a0dce
sangitha <sangiekaruna07@gmail.com>
2023-08-30 18:46:51 +0530

    Initial commit

---
+++ demo2346/src/main/java/com/example/demo2346/Demo2346Application.java
@@ -0,0 +1,13 @@
+package com.example.demo2346;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2346Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2346Application.class, args);
+	}
+
+}
---
+++ demo2346/src/test/java/com/example/demo2346/Demo2346ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo2346;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2346ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
